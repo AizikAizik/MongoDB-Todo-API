0391cac9bf95bfff322c60474c03c72e8163822b
Aizik_Anonymous <isaac.ogunleye@hotmail.com>
2020-11-04 17:05:14 +0100

    added a user collection and inserted many documents inside the collection

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -8,12 +8,57 @@ MongoClient.connect(URL, (err, client) =>{
 
     console.log('Connected to the MongoDb service successfully!!');
     const db = client.db('TodoApp');
-    db.collection('Todos')
-        .insertOne({
-            text : "Buy milk today",
-             completed : false,
-              DateTimeAdded : new Date()
-            },
+
+    //add Todo Collection
+    // db.collection('Todos')
+    //     .insertMany(
+    //         [
+    //             {
+    //                 text : "Buy milk today",
+    //                 completed : false,
+    //                 DateTimeAdded : new Date()
+    //             },
+    //             {
+    //                 text : "Visit Girlfriend",
+    //                 completed : false,
+    //                 DateTimeAdded : new Date()
+    //             },
+    //             {
+    //                 text : "Read Flutter text book",
+    //                 completed : false,
+    //                 DateTimeAdded : new Date()
+    //             }
+
+    //         ],
+    //         (err, result) =>{
+    //             if (err){
+    //                 return console.log('Unable to Insert todo document', err)
+    //             }
+    //             console.log(JSON.stringify(result.ops, null, 2));
+    //         }
+    //     )
+
+    //add User Collection
+    db.collection('Users')
+        .insertMany(
+            [
+                {
+                    firstName : "Jacob",
+                    lastName : "Oniel",
+                    password : `jacob123`
+                },
+                {
+                    firstName : "Sarah",
+                    lastName : "Williams",
+                    password : `sarasara`
+                },
+                {
+                    firstName : "Isaac",
+                    lastName : "Ogunleye",
+                    password : `AizikAizik`
+                },
+
+            ],
             (err, result) =>{
                 if (err){
                     return console.log('Unable to Insert todo document', err)
@@ -21,6 +66,6 @@ MongoClient.connect(URL, (err, client) =>{
                 console.log(JSON.stringify(result.ops, null, 2));
             }
         )
-        
+
     client.close();
 })
