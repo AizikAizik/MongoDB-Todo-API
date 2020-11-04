f2e4269d4c751821f8a9f87765fb0feafb264430
Aizik_Anonymous <isaac.ogunleye@hotmail.com>
2020-11-04 17:06:22 +0100

    uncommented the commented code

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -9,34 +9,34 @@ MongoClient.connect(URL, (err, client) =>{
     console.log('Connected to the MongoDb service successfully!!');
     const db = client.db('TodoApp');
 
-    //add Todo Collection
-    // db.collection('Todos')
-    //     .insertMany(
-    //         [
-    //             {
-    //                 text : "Buy milk today",
-    //                 completed : false,
-    //                 DateTimeAdded : new Date()
-    //             },
-    //             {
-    //                 text : "Visit Girlfriend",
-    //                 completed : false,
-    //                 DateTimeAdded : new Date()
-    //             },
-    //             {
-    //                 text : "Read Flutter text book",
-    //                 completed : false,
-    //                 DateTimeAdded : new Date()
-    //             }
+    add Todo Collection
+    db.collection('Todos')
+        .insertMany(
+            [
+                {
+                    text : "Buy milk today",
+                    completed : false,
+                    DateTimeAdded : new Date()
+                },
+                {
+                    text : "Visit Girlfriend",
+                    completed : false,
+                    DateTimeAdded : new Date()
+                },
+                {
+                    text : "Read Flutter text book",
+                    completed : false,
+                    DateTimeAdded : new Date()
+                }
 
-    //         ],
-    //         (err, result) =>{
-    //             if (err){
-    //                 return console.log('Unable to Insert todo document', err)
-    //             }
-    //             console.log(JSON.stringify(result.ops, null, 2));
-    //         }
-    //     )
+            ],
+            (err, result) =>{
+                if (err){
+                    return console.log('Unable to Insert todo document', err)
+                }
+                console.log(JSON.stringify(result.ops, null, 2));
+            }
+        )
 
     //add User Collection
     db.collection('Users')
