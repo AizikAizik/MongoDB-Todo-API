d5a97b0741c28e6f93da27f1137603d0a59bdd69
Aizik_Anonymous <isaac.ogunleye@hotmail.com>
2020-11-18 17:01:06 +0100

    Added patch and delete routing functionalities

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const _ = require('lodash')
+const {ObjectID} = require('mongodb')
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -16,17 +18,107 @@ app.post('/todos', (req, res) =>{
 
     //create a new todo item
     const todo = new Todo({
-        text 
-    })
+        text
+    });
 
     //add the todo to the database
     todo.save()
-        .then(doc =>{
-            res.send(doc)
+        .then(todo =>{
+            console.log(req.ip);
+            res.send({todo})
         })
         .catch(err =>{
             res.status(400).send(err)
         })
+})
+
+//todo route (GET method)
+//fetch all todos
+app.get('/todos', (req, res) =>{
+    Todo.find()
+        .then( todos => {
+            res.send({todos})
+        } )
+        .catch((err) => {
+            res.status(400).send(err.message);
+        })
+})
+
+//todo/id route (GET method)
+//get a specific todo by a parameter id passed in the Url
+app.get('/todos/:id', (req, res) =>{
+    const {id} = req.params;
+
+    if(ObjectID.isValid(id)){
+        Todo.findById(id)
+            .then(todo =>{
+                if(todo){
+                    res.json({todo})
+                }else{
+                    res.status(404).send({error : 'ID does not exist in the DB'})
+                }
+            })
+    }else{
+        res.status(400).send({error : 'ID is not Valid!!'})
+    }
+})
+
+//todo/id route (DELETE method)
+//delete a specific todo id passed in the URL
+app.delete('/todos/:id', (req, res) =>{
+    const {id} = req.params;
+
+    if(ObjectID.isValid(id)){
+        Todo.findByIdAndRemove(id)
+            .then(todo =>{
+                if(todo){
+                    console.log(`Todo removed!!`)
+                    res.json({todo})
+                }else{
+                    res.status(404).send({error : 'ID does not exist in the DB'})
+                }
+            })
+    }else{
+        res.status(400).send({error : 'ID is not Valid!!'})
+    }
+})
+
+//todo/id route (PATCH method)
+// update a specific todo data (text or completed value)
+app.patch('/todos/:id', (req, res) =>{
+    const {id} = req.params;
+    //only pick two properties to update from request.body
+    const bodyData = _.pick(req.body, ['text', 'completed']);
+    const isIdValid = ObjectID.isValid(id)
+    if(!isIdValid){
+        return res.status(400).send({error : 'ID is not Valid!!'})
+    }
+    // destructure 2 variables from object
+    let {text, completed} = bodyData;
+    // check if value of completed is a Boolean or not
+    const isCompletedABoolean = _.isBoolean(completed);
+    if(isCompletedABoolean && completed){
+        bodyData.completedAt = new Date().getTime()
+    }else{
+        bodyData.completedAt = null;
+        bodyData.completed = false;
+    }
+
+    //update the TODO by the ID
+    Todo.findByIdAndUpdate(id,
+        {$set: bodyData},
+        {new: true}
+    ).then((todo) => {
+        if(todo){
+            console.log('Todo Updated!!');
+            return res.send({todo})
+        }else{
+            return res.status(404).send({msg : 'Todo not Found'})
+        }
+    }).catch((err) => {
+        console.log(`${err.message}`)
+        res.status(500).send({msg : 'Server DB Error'})
+    })
 })
 
 const port = process.env.PORT || 5050;
